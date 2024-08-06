ed40b1ab218d4f75d4068bc4a748951f77845188
kevin <kevin.mazure.km@gmail.com>
2024-08-06 16:37:50 +0200

    first Commit

---
+++ src/test/java/com/example/SpringAllTest/SpringAllTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringAllTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAllTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
